a804fe8b385a76aa02068c63641e326d5822281c
Nicolas Grimault <nicolas.g21@live.fr>
2018-07-19 23:56:47 +0200

    Add Types

---
+++ Type.cs
@@ -0,0 +1,24 @@
+namespace Pokemon
+{
+    public enum @Type : int
+    {
+        Normal = 0,
+        Fighting = 1,
+        Flying = 2,
+        Poison = 3,
+        Ground = 4,
+        Rock = 5,
+        Bug = 6,
+        Ghost = 7,
+        Steel = 8,
+        Fire = 9,
+        Water = 10,
+        Grass = 11,
+        Electric = 13,
+        Psychic = 14,
+        Ice = 15,
+        Dragon = 16,
+        Dark = 17,
+        Fairy = 18
+    }
+}
